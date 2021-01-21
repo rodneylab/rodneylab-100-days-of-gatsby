e1a8d19495036bac5c995a672bc3c2e3e203fa97
Rodney Lab <codesign@rodneylab.com>
2021-01-21 19:16:04 +0000

    feat(ux): 🧑🏽 added a bit of background on the purpose of the site to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,21 @@ const Footer = () => (
     Page created by
     {' '}
     <a
-      aria-label="Open Rodney Lac contact page"
+      aria-label="Open Rodney Lab contact page"
       href="https://rodneylab.com/contact"
       rel="nofollow noopener noreferrer"
     >
       Rodney Lab
+    </a>
+    {' '}
+    as part of
+    {' '}
+    <a
+      aria-label="See Rodney Lab post on the challenge"
+      href="https://rodneylab.com/100-days-of-gatsby-code-2021/"
+      rel="nofollow nopener noreferrer"
+    >
+      100 Days of Gatsby Code 2021
     </a>
     .
   </footer>
