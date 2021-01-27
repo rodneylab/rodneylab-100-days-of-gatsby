f77837e852b26aac2b7eebe3c96562a7131d7af5
Rodney Lab <codesign@rodneylab.com>
2021-01-27 11:01:33 +0000

    style: 🛁 updated eslint config for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es2020: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'plugin:cypress/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,6 +17,6 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['react', 'jsx-a11y'],
+  plugins: ['react', 'jsx-a11y', 'cypress'],
   rules: {},
 };
