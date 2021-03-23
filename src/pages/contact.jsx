acd0d3b41995e4c4bf6f802640340f17f5fecde5
Rodney Lab <codesign@rodneylab.com>
2021-03-23 11:51:12 +0000

    fix: 🤗 fixed accessibility issue with headers

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,6 +12,7 @@ import {
   ListItem,
   Text,
   useMediaQuery,
+  VisuallyHidden,
 } from '@chakra-ui/react';
 import { FormiumForm } from '@formium/react';
 import PropTypes from 'prop-types';
@@ -135,6 +136,9 @@ export default function Contact({ pageContext: { slug }, data }) {
         <SEO data={data} pageMetadata={pageMetadata} />
         <Layout data={data}>
           <Flex as="main" w="100%" justify="stretch" direction="column" color="yellow.500">
+            <VisuallyHidden>
+              <Heading as="h1">Contact us</Heading>
+            </VisuallyHidden>
             <Grid templateColumns="1fr 40% 40% 1fr" templateRows="auto auto" py="12">
               <GridItem colSpan={1} rowSpan={1} />
               <GridItem bg="blue.700" colSpan={2} rowSpan={1}>
@@ -188,6 +192,9 @@ export default function Contact({ pageContext: { slug }, data }) {
       <SEO data={data} pageMetadata={pageMetadata} />
       <Layout data={data}>
         <Flex as="main" direction="column" w="100%" color="yellow.500" px={[4, 4, 'auto', 4, 0]}>
+          <VisuallyHidden>
+            <Heading as="h1">Contact us</Heading>
+          </VisuallyHidden>
           <Flex bg="blue.700" justify="flex-start" direction="column">
             <Grid templateColumns="1fr 80% 1fr" templateRows="auto" py="12">
               <GridItem colSpan={1} />
