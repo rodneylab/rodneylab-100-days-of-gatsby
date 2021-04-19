4440deb60bba5e22b96dff990d4f1c4acfc2c18c
Rodney Lab <codesign@rodneylab.com>
2021-04-19 17:23:49 +0100

    feat: ✨ added Breadcrumbs component

---
+++ src/components/Breadcrumbs.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
+import { ArrowRightIcon } from '@chakra-ui/icons';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+
+const Breadcrumbs = ({ items }) => {
+  const lastIndex = items.length - 1;
+
+  return (
+    <Breadcrumb my={4} px={4} w="100%" separator={<ArrowRightIcon boxSize={2}/>}>
+      {items.map((item, index) => (
+        <BreadcrumbItem isCurrentPage={index === lastIndex}>
+          <BreadcrumbLink as={Link} to={item.to}>
+            {item.name}
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+      ))}
+    </Breadcrumb>
+  );
+};
+
+Breadcrumbs.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      to: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export { Breadcrumbs as default };
